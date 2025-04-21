8af74b098b8a1b2fc753157b18d56f5ae8dc58f9
danie <3yuste3@gmail.com>
2025-04-21 18:04:29 +0200

    clase main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,21 @@
+import database.DataBaseManager;
+import program.Authenticator;
+import program.OrderManager;
+import program.PaymentProcessor;
+
 public class Main {
+    public static void main(String[] args) {
+        DataBaseManager dbManager = new DataBaseManager();
+        dbManager.connect();
+
+        PaymentProcessor paymentProcessor = new PaymentProcessor();
+        Authenticator authenticator = new Authenticator(dbManager);
+        OrderManager orderManager = new OrderManager(dbManager, paymentProcessor);
+
+        if (authenticator.authenticate("admin", "1234")) {
+            orderManager.createOrder("admin", "Pizza Hawaiana con borde de queso", 18.99);
+        } else {
+            System.out.println("Autenticación fallida.");
+        }
+    }
 }
